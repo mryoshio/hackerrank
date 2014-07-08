d4ff8f6458aea921a336dc6a0438ccb9e99809d0
mryoshio <yoshiokaas@gmail.com>
2014-07-08 22:48:32 +0900

    add memo function to service_lane

--- cpp/service_lane.cpp
+++ cpp/service_lane.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <array>
 
+#define N_MAX 100000
+
 using namespace std;
 
 int N, T;
 array<int, 100000> widths;
+int memo[N_MAX + 1][N_MAX + 1] = { 0 };
 
 void solve() {
   int s, e;
   for (int i = 0; i < T; i++) {
     int v = 3;
     cin >> s >> e;
-    for (; s < e; s++)
-      if (v > widths.at(s)) v = widths.at(s);
-    cout << v << endl;
+    if (memo[s][e] > 0) {
+      cout << memo[s][e] << endl;
+      continue;
+    }
+    for (int i = s; i < e; i++)
+      if (v > widths.at(i)) v = widths.at(i);
+    cout << (memo[s][e] = v) << endl;
   }
 }
 
