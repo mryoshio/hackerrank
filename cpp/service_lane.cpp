4e57a458b8922f955ae42d26598f1cca3c644cb1
mryoshio <ashitaba.yoshioka@leonisand.co>
2014-07-08 12:18:09 +0900

    update service lane

--- cpp/service_lane.cpp
+++ cpp/service_lane.cpp
@@ -11,24 +11,15 @@ void solve() {
   for (int i = 0; i < T; i++) {
     int v = 3;
     cin >> s >> e;
-    while (s <= e) {
+    for (; s < e; s++)
       if (v > widths.at(s)) v = widths.at(s);
-      s++;
-    }
     cout << v << endl;
   }
-}
-
-void dump() {
-  cout << N << endl;
-  cout << T << endl;
-  for (int i = 0; i < N; i++) cout << widths[i] << endl;
 }
 
 int main() {
   cin >> N >> T;
   for (int i = 0; i < N; i++) cin >> widths[i];
-  // dump();
   solve();
   return 0;
 }
