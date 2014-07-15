d6a6608d6669c29c7c241d1230474a7d7c56533e
mryoshio <yoshiokaas@gmail.com>
2014-07-16 01:24:25 +0900

    update llmystery

--- cpp/loveletter_mystery.cpp
+++ cpp/loveletter_mystery.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int T;
-
-bool is_palindrome(string s) {
-  bool ret = true;
-  for (int i = 0; i < s.size()/2; i++)
-    if (s[i] != s[s.size()-1-i])
-      return false;
-  return true;
-}
-
 void solve() {
-  string s;
-
-  for (int i = 0; i < T; i++) {
-    int cnt = 0, idx = 0;
+  int t;
+  cin >> t;
+  while(t--) {
+    int cnt = 0;
+    string s;
     cin >> s;
-    do {
-      if (s[idx] == s[s.size()-1-idx] || s[s.size()-1-idx] == 'a') {
-        idx++;
-      } else {
-        s[s.size()-1-idx]--;
-        cnt++;
-      }
-    } while (!is_palindrome(s));
+    for (int i = 0; i < s.size()/2; i++)
+      cnt += abs(s[i] - s[s.size()-1-i]);
     cout << cnt << endl;
   }
 }
 
 int main() {
-  cin >> T;
   solve();
   return 0;
 }
